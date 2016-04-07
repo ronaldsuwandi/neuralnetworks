3e674797ce38c003271c49b3c0b5cd0a7dd868f4
Ronald Suwandi <ronald@ronaldsuwandi.com>
2016-04-07 21:52:39 +0800

    Updated project url and name

--- project.clj
+++ project.clj
@@ -1,13 +1,14 @@
-(defproject neuralnetworks "0.1.0-SNAPSHOT"
+(defproject ronaldsuwandi/neuralnetworks "0.1.0-SNAPSHOT"
   :description "Neural networks library for Clojure. Built on top of core.matrix"
-  :url "http://example.com/FIXME"
+  :url "https://ronaldsuwandi.github.io/neuralnetworks/"
   :license {:name "MIT License"
             :url  "https://opensource.org/licenses/MIT"}
   :plugins [[lein-codox "0.9.4"]
             [lein-kibit "0.1.2"]
             [jonase/eastwood "0.2.3"]
             [lein-bikeshed "0.3.0"]]
-  :codox {:metadata {:doc/format :markdown}}
+  :codox {:metadata   {:doc/format :markdown}
+          :source-uri "https://github.com/ronaldsuwandi/neuralnetworks/blob/master/{filepath}#L{line}"}
   :profiles {:test
              {:dependencies [[criterium/criterium "0.4.4"]]}}
   :aliases {"quality" ["do"
